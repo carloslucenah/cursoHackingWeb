966d4ebdf9275b78493f4a417e5403117afd47d2
carloslucenah <carloslucenah@gmail.com>
2020-04-13 09:14:48 +0200

    Cambiado usuario de base de datos
    Cambiado usuario de base de datos, de "foro" a "root" sin password.

--- foro/config/config.php
+++ foro/config/config.php
@@ -1,8 +1,8 @@
 <?php
 //DB Params
 define ("DB_HOST", "localhost");
-define ("DB_USER", "foro");
-define ("DB_PASS", "foro");
+define ("DB_USER", "root");
+define ("DB_PASS", "");
 define ("DB_NAME", "forum");
 
 define("SITE_TITLE", "Bienvenido al foro!");
